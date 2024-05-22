15642dada41b692ce54db1f02594dcf986d2c169
Mark Kpodo <alimark96@yahoo.com>
2024-05-22 10:03:12 +0100

    Add files via upload

---
+++ 05741/src/java/org/net/U8.java
@@ -0,0 +1,46 @@
+package org.net;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "U8", urlPatterns = {"/U8"})
+public class U8 extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+
+            String login = request.getParameter("login");
+            String password = request.getParameter("password");
+            if ("admin".equals(login) && "password".equals(password)) {
+                out.println("Welcome");
+            } else {
+                out.println("Wrong/Logins");
+            }
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
